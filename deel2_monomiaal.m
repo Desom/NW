00f152df6213033465180262b3f94cbb79b8697a
Desom <gertjan.franken@gmail.com>
2014-04-20 14:07:51 +0200

    Verder werken aan deel 2

--- deel2_monomiaal.m
+++ deel2_monomiaal.m
@@ -1,5 +1,11 @@
-[A, b] = stelsel_monomiaal(@func1, 20);
+n = 20;
+[A, b] = stelsel_monomiaal(@func1, n);
 
+e = zeros(1,n);
 for i=1:n
-    a = A(:i,:i)\b;
+    a = A(1:i,1:i)\b(1:i);
+    func = @(x) (func1(x) - polyval(fliplr(a),x)) .^ 2;
+    e(i) = sqrt(quad(func, -1, 1, 1e-8));
 end
+
+plot(e)
