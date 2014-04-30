b576e2fd9001cc49e8ddc23b2ba44638216acf6f
Desom <gertjan.franken@gmail.com>
2014-04-30 22:27:17 +0200

    Verbeteringen

--- deel1.m
+++ deel1.m
@@ -3,11 +3,11 @@
 % Eerste functie
 uitkomst1 = exp(1) - exp(-1);
 
-trapezium_integratie_fout1 = zeros(1,100);
-simpson_integratie_fout1 = zeros(1,100);
+trapezium_integratie_fout1 = zeros(1,200);
+simpson_integratie_fout1 = zeros(1,200);
 h1 = zeros(1,100);
 
-for i=1:100
+for i=1:200
     n = i * 100;
     trapezium_integratie_fout1(i) = abs(uitkomst1 - trapezium(@func1,-1,1,n));
     simpson_integratie_fout1(i) = abs(uitkomst1 - simpson(@func1,-1,1,n));
@@ -15,12 +15,16 @@ for i=1:100
 end
 
 figure(1)
+semilogy(h1, h1.^2, 'r');
+hold
 semilogy(h1,trapezium_integratie_fout1)
 title('Trapeziumregel, eerste functie')
 xlabel('h')
 ylabel('integratiefout')
 
 figure(2)
+semilogy(h1, h1.^4, 'r');
+hold
 semilogy(h1,simpson_integratie_fout1)
 title('Regel van Simpson, eerste functie')
 xlabel('h')
@@ -33,7 +37,7 @@ trapezium_integratie_fout2 = zeros(1,100);
 simpson_integratie_fout2 = zeros(1,100);
 h2 = zeros(1,100);
 
-for i=1:100
+for i=1:200
     n = i * 100;
     trapezium_integratie_fout2(i) = abs(uitkomst2 - trapezium(@func2,-5,5,n));
     simpson_integratie_fout2(i) = abs(uitkomst2 - simpson(@func2,-5,5,n));
@@ -41,12 +45,16 @@ for i=1:100
 end
 
 figure(3)
+semilogy(h2, h2.^2, 'r');
+hold
 semilogy(h2,trapezium_integratie_fout2)
 title('Trapeziumregel, tweede functie')
 xlabel('h')
 ylabel('integratiefout')
 
 figure(4)
+semilogy(h2, h2.^4, 'r');
+hold
 semilogy(h2,simpson_integratie_fout2)
 title('Regel van Simpson, tweede functie')
 xlabel('h')
@@ -54,24 +62,28 @@ ylabel('integratiefout')
 
 %% 1.2 Adaptieve routine
 
-ewaarden = zeros(1,14);
-for i=1:14
+ewaarden = zeros(4,14);
+for i=4:14
     ewaarden(i) = 10^(-i);
 end
 
 a = -5;
 b = 5;
 
-trapezium_tijden = zeros(1,14);
-simpson_tijden = zeros(1,14);
-quad_tijden = zeros(1,14);
+trapezium_tijden = zeros(4,14);
+simpson_tijden = zeros(4,14);
+quad_tijden = zeros(4,14);
 
-for i=1:14
+for i=4:14
     trapezium_tijden(i) = calculateTime(@trapezium_adaptief, @func2, a, b, ewaarden(i));
     simpson_tijden(i) = calculateTime(@simpson_adaptief, @func2, a, b, ewaarden(i));
     quad_tijden(i) = calculateTime(@quad, @func2, a, b, ewaarden(i));
 end
 
+figure(5)
+subplot(3,1,1)
 semilogx(ewaarden,trapezium_tijden)
+subplot(3,1,2)
 semilogx(ewaarden,simpson_tijden)
+subplot(3,1,3)
 semilogx(ewaarden,quad_tijden)
--- deel2_monomiaal.m
+++ deel2_monomiaal.m
@@ -18,21 +18,25 @@ n = 20;
 [ A, b ] = stelsel_monomiaal(@func1, n);
 [ Aster, bster ] = stelsel_monomiaal_exp(n);
 
-e_a = zeros(n);
-e_b = zeros(n);
-e_A = zeros(n);
+e_a = zeros(1,n);
+e_b = zeros(1,n);
+e_A = zeros(1,n);
+cond_A = zeros(1,n);
 for i=1:n
     a = A(1:i,1:i)\b(1:i);
     aster = Aster(1:i,1:i)\bster(1:i);
     e_a(i) = norm(a - aster) / norm(aster);
     e_b(i) = norm(b(1:i) - bster(1:i)) / norm(bster(1:i));
     e_A(i) = norm(A(1:i) - Aster(1:i)) / norm(Aster(1:i));
+    cond_A(i) = cond(A(1:i,1:i));
 end
 
 figure
-subplot(3,1,1)
+subplot(2,1,1)
 semilogy(e_b)
-subplot(3,1,2)
-semilogy(e_A)
-subplot(3,1,3)
+hold
+semilogy(e_A, 'r')
+subplot(2,1,2)
 semilogy(e_a)
+hold
+semilogy(cond_A .* (1e-8), 'r')
